6e01edfc3ae7162caff50cca54f8cc60921217fc
Jordan Hart <jordandhart@gmail.com>
2018-01-24 23:30:30 -0800

    added more test variables

---
+++ newer_work_without_encryption/client.py
@@ -0,0 +1,200 @@
+#udp cleint from https://docs.python.org/3/library/asyncio-protocol.html#udp-echo-client-protocol
+
+import asyncio as asyncio
+import time
+import queue
+import json
+import reedsolo
+
+
+fps = [30, 30, 30]
+tick_length = 1
+timeout_time = 5
+e = 1
+time_delay_client_timestamps = 0
+
+def data_generator(original_time, fps):
+  current_time = time.time()
+  lst = []
+  for index in range(len(fps)):
+    f = fps[index]
+    for i in range(f):
+      #use i/f instead of wait(1/f) after list append for code to run faster
+      lst.append([int((current_time + index - original_time + i/f + time_delay_client_timestamps))//tick_length, 100])
+  return lst
+
+def local_data(original_time=None):
+    data = data_generator(original_time, fps)
+    pq = queue.PriorityQueue(maxsize = 2000)
+    pqs.insert(0,pq)#tuples empty without this. Why doesnt time_sync work when reference_queue isnt local queue?
+    add_packets_to_queue(pq, data)
+    return original_time
+def time_sync():
+    t = []
+    if len(pqs) == 0:
+        return t
+    if len(pqs) == 1:
+        return list(pqs[0].queue)
+    #while queue_not_empty(pqs) == True:
+    reference_queue = pqs.pop()
+    while reference_queue.qsize() > 0:
+            reference_packet = reference_queue.get()
+            timeout = time.time() + timeout_time
+            for q in pqs:
+                if timeout - time.time() >= 0:
+                    q_packet = sync_packet(reference_packet, q)
+                    if q_packet != None:
+                        t.append((reference_packet, q_packet))
+    return t
+
+def queue_not_empty(lst_queues):
+    for q in lst_queues:
+        if q.qsize() > 0:
+            return True
+    return False
+def sync_packet(reference_packet, queue):
+    for p in queue.queue:
+        if abs(p.time - reference_packet.time) <= e :
+            queue.get(p)
+            return p
+        else:
+            print("not here")
+    return None
+
+def add_packets_to_queue(queue, data):
+    for each_tuple in data:
+        queue.put(packet(each_tuple[0], each_tuple[1]))        
+#only one queue here in current test
+
+class packet(object):
+    def __init__(self, time, data):
+        self.time = time
+        self.data = data
+    def __eq__(self, other):
+        return other != None or  self.time == other.time
+    def __ne__(self,other):
+        return other == None or self.time != other.time
+    def __lt__(self,other):
+        return self.time < other.time
+    def __le__(self,other):
+        return self.time <= other.time
+    def __gt__(self,other):
+        return self.time > other.time
+    def __ge__(self,other):
+        return self.time >= other.time
+    def __repr__(self):
+        return "time" + ": "+str(self.time) + ", data: " + str(self.data)
+
+class EchoClientProtocol:
+    def __init__(self, message, loop, tick_length = 1):
+        self.message = message
+        self.loop = loop
+        self.transport = None
+        self.connection_time = time.time()
+        self.tick_length = tick_length
+        self.sync_latency = {}
+        self.qs = {}
+
+    def connection_made(self, transport):
+        print("udp connction made")
+        self.transport = transport
+        print('Send:', self.message)
+        self.transport.sendto("request data".encode())
+
+    def datagram_received(self, data, addr):
+        global tuples
+        global time_data_recieved_start
+        time_data_recieved_start = time.time()
+        rs = reedsolo.RSCodec(10)
+        json_data =  rs.decode(data)
+        original_message = json.loads(json_data)
+        print("recieved data", message)
+        if addr not in self.qs.keys():
+            self.qs[addr] = queue.PriorityQueue(maxsize = 2000)
+            pqs.append(self.qs[addr])
+        print("recieved data")
+        add_packets_to_queue(self.qs[addr], original_message)
+        tuples = time_sync()
+        analyze_tuples(tuples)
+        print("Close the socket")
+        # self.transport.close()
+    def get_time():
+        return (time.time() - original_time) // tick_length
+    def get_time_from_message(message):
+        return int(message)
+    def error_received(self, exc):
+        print('Error received:', exc)
+
+
+class ControllerClientProtocol(asyncio.Protocol):
+    def __init__(self, message, loop):
+        self.message = message
+        self.loop = loop
+
+
+    def connection_made(self, transport):
+        global time_timesync_started
+        self.transport = transport
+        self.transport.write("request to sync time".encode())
+        self.sync_time = time.time()
+        time_timesync_started = time.time()
+        #transport.write(self.message.encode())
+        #print('Data sent: {!r}'.format(self.message))
+
+    def data_received(self, data):
+        global time_timesync_ended
+        message = data.decode()
+        print(message)
+        psuedo_time = float(message)
+        latency = (time.time() - self.sync_time) / 2
+        original_time = (time.time()- latency - psuedo_time)
+        local_data(original_time)
+        time_timesync_ended = time.time()
+        self.transport.close()
+
+
+    def connection_lost(self, exc):
+        print('The server closed the connection')
+        print('Stop the event loop')
+        self.loop.stop()
+def analyze_tuples(t):
+    print("tuples: ", t)
+    print("client total tuples of frames: ", len(t))
+    print("average tuples / frames per second combining local and remote: ",len(t)/ (len(fps)))
+    print("seconds since recieved data: ", time.time() - time_data_recieved_start)
+    print("seconds for experiment to run", time.time() - protocol_start_time)
+    print("seconds took for time handshake: ", time_timesync_ended - time_timesync_started)
+
+
+pqs=[]
+original_time = None
+time_data_recieved_start = None
+tuples = None
+time_timesync_ended = None
+time_timesync_started = None
+protocol_start_time = time.time()
+
+
+
+
+loop = asyncio.get_event_loop()
+message = 'Hello World!'
+coro = loop.create_connection(lambda: ControllerClientProtocol(message, loop),
+                              '127.0.0.1', 8889)
+client = loop.run_until_complete(coro)
+loop.run_forever()
+loop.close()
+
+
+loop = asyncio.new_event_loop()
+message = "udp message!"
+udp_time_start = time.time()
+connect = loop.create_datagram_endpoint(
+    lambda: EchoClientProtocol(message, loop),
+    remote_addr=('127.0.0.1', 9999))
+transport, protocol = loop.run_until_complete(connect)
+loop.run_forever()
+transport.close()
+loop.close()
+
+
---
+++ newer_work_without_encryption/transmitter.py
@@ -0,0 +1,118 @@
+#udp server from https://docs.python.org/3/library/asyncio-protocol.html#udp-echo-client-protocol
+
+import asyncio
+import time
+import json
+import reedsolo
+
+pqs=[]
+fps = [30, 30, 30]
+tick_length = 1
+time_delay_transmitter_timestamps = 0
+network_delay_transmitter_to_client_over_udp = 0
+
+data = None
+json_data = None
+
+
+def data_generator(original_time, fps):
+  current_time = time.time()
+  lst = []
+  for index in range(len(fps)):
+    f = fps[index]
+    for i in range(f):
+      #use i/f instead of wait(1/f) after list append for code to run faster
+      lst.append([int((current_time + index - original_time + i/f + time_delay_transmitter_timestamps))//tick_length, 100])
+  return lst
+
+
+class EchoServerProtocol:
+    def connection_made(self, transport):
+        self.transport = transport
+        self.connection_made_time = time.time() // tick_length
+        self.tick_length = tick_length
+        self.original_time = {}
+        print("json_data in echoserver not None", json_data != None)
+
+    def datagram_received(self, data, addr):
+        message = data.decode()
+        print('Received %r from %s' % (message, addr))
+        print("sending json_data", json_data)
+        rs = reedsolo.RSCodec(10)
+        reed_data = rs.encode(json_data)
+        if network_delay_transmitter_to_client_over_udp > 0:
+            time.sleep(network_delay_transmitter_to_client_over_udp)
+        self.transport.sendto(reed_data, addr)
+        # print('Received %r from %s' % (message, addr))
+        # print('Send %r to %s' % (message, addr))
+        # self.transport.sendto(data, addr)
+    
+
+
+
+class EchoServerControllerProtocol(asyncio.Protocol):
+    def connection_made(self, transport):
+        peername = transport.get_extra_info('peername')
+        print('Connection from {}'.format(peername))
+        self.transport = transport
+        self.tick_length = tick_length
+
+    def data_received(self, data):
+        global json_data
+        message = data.decode()
+        if (self.request_to_sync_message(message)):
+            self.transport.write(str((time.time() - original_time)//self.tick_length).encode())
+        print('Data received: {!r}'.format(message))
+
+        print('Send: {!r}'.format(message))
+        # self.transport.write(data)
+        data = data_generator(original_time, fps)
+        json_data = json.dumps(data)
+        print("json_data", json_data)
+        print("json_data in control server not None: ", json_data!= None)
+        
+        print('Close the client socket')
+        self.transport.close()
+    def connection_lost(self, exc):
+        # The socket has been closed, stop the event loop
+        loop.stop()
+
+
+        # print('Close the client socket')
+        # self.transport.close()
+    def request_to_sync_message(self, message):
+        return message == "request to sync time"
+original_time = time.time()
+loop = asyncio.get_event_loop()
+print("starting tcp server")
+# Each client connection will create a new protocol instance
+coro = loop.create_server(EchoServerControllerProtocol, '127.0.0.1', 8889)
+server = loop.run_until_complete(coro)
+
+# Serve requests until Ctrl+C is pressed
+# print('Serving on {}'.format(server.sockets[0].getsockname()))
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
+# # Close the server
+server.close()
+loop.run_until_complete(server.wait_closed())
+loop.close()
+
+loop = asyncio.new_event_loop()
+print("Starting UDP server")
+# One protocol instance will be created to serve all client requests
+
+listen = loop.create_datagram_endpoint(
+    EchoServerProtocol, local_addr=('127.0.0.1', 9999))
+transport, protocol = loop.run_until_complete(listen)
+
+try:
+    loop.run_forever()
+except KeyboardInterrupt:
+    pass
+
+transport.close()
+loop.close()
