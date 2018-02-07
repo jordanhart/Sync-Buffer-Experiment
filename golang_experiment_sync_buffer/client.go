597289a5135d384f5123ca082d9821c7c141a05a
Jordan Hart <jordandhart@gmail.com>
2018-02-07 14:57:25 -0800

    notify working

--- golang_experiment_sync_buffer/client.go
+++ golang_experiment_sync_buffer/client.go
@@ -26,12 +26,50 @@ func main() {
 	psuedotime_string := resp.GetMessage().GetPayload().GetBytes()
     var offset time.Duration
 	json.Unmarshal(psuedotime_string, &offset)
-	// t := time.Microsecond
-    finish := time.Now()
+    finish_timesync := time.Now()
     
 	fmt.Println("offset in seconds : ")
 	fmt.Println(offset.Seconds())
 	fmt.Println("Time lapse: ")
-	fmt.Println(finish.Sub(start))
+	fmt.Println(finish_timesync.Sub(start))
+	
+	tok, err := conn.ObserveResource("/watch/this")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	obsChannel := make(chan canopus.ObserveMessage)
+	done := make(chan bool)
+	go conn.Observe(obsChannel)
+
+	notifyCount := 0
+	go func() {
+		for {
+			select {
+			case obsMsg, open := <-obsChannel:
+				if open {
+					if notifyCount == 5 {
+						fmt.Println("[CLIENT >> ] Canceling observe after 5 notifications..")
+						go conn.CancelObserveResource("watch/this", tok)
+						go conn.StopObserve(obsChannel)
+						done <- true
+						return
+					} else {
+						notifyCount++
+						// msg := obsMsg.Msg\
+						resource := obsMsg.GetResource()
+						val := obsMsg.GetValue()
+
+						fmt.Println("[CLIENT >> ] Got Change Notification for resource and value: ", notifyCount, resource, val)
+					}
+				} else {
+					done <- true
+					return
+				}
+			}
+		}
+	}()
+	<-done
+	fmt.Println("Done")
 
 }
--- golang_experiment_sync_buffer/server.go
+++ golang_experiment_sync_buffer/server.go
@@ -1,39 +1,41 @@
 package main
 
 import (
-	"log"
+    "fmt"
     "time"
 	"github.com/zubairhamed/canopus"
 	"encoding/json"
+	"math/rand"
+	"strconv"
 )
 
 func main() {
 	server := canopus.NewServer()
 	psuedo_time_start := time.Now()
 
-	server.Get("/hello", func(req canopus.Request) canopus.Response {
-		log.Println("Hello Called")
-		msg := canopus.ContentMessage(req.GetMessage().GetMessageId(), canopus.MessageAcknowledgment)
-		msg.SetStringPayload("Acknowledged with response : " + req.GetMessage().GetPayload().String())
+	// server.Get("/hello", func(req canopus.Request) canopus.Response {
+	// 	log.Println("Hello Called")
+	// 	msg := canopus.ContentMessage(req.GetMessage().GetMessageId(), canopus.MessageAcknowledgment)
+	// 	msg.SetStringPayload("Acknowledged with response : " + req.GetMessage().GetPayload().String())
 
-		res := canopus.NewResponse(msg, nil)
-		return res
-	})
+	// 	res := canopus.NewResponse(msg, nil)
+	// 	return res
+	// })
 
-	server.Post("/hello", func(req canopus.Request) canopus.Response {
-		log.Println("Hello Called via POST")
+	// server.Post("/hello", func(req canopus.Request) canopus.Response {
+	// 	log.Println("Hello Called via POST")
 
-		msg := canopus.ContentMessage(req.GetMessage().GetMessageId(), canopus.MessageAcknowledgment)
-		msg.SetStringPayload("Acknowledged: " + req.GetMessage().GetPayload().String())
-		res := canopus.NewResponse(msg, nil)
-		return res
-	})
+	// 	msg := canopus.ContentMessage(req.GetMessage().GetMessageId(), canopus.MessageAcknowledgment)
+	// 	msg.SetStringPayload("Acknowledged: " + req.GetMessage().GetPayload().String())
+	// 	res := canopus.NewResponse(msg, nil)
+	// 	return res
+	// })
 
-	server.Get("/basic", func(req canopus.Request) canopus.Response {
-		msg := canopus.NewMessageOfType(canopus.MessageAcknowledgment, req.GetMessage().GetMessageId(), canopus.NewPlainTextPayload("Acknowledged"))
-		res := canopus.NewResponse(msg, nil)
-		return res
-	})
+	// server.Get("/basic", func(req canopus.Request) canopus.Response {
+	// 	msg := canopus.NewMessageOfType(canopus.MessageAcknowledgment, req.GetMessage().GetMessageId(), canopus.NewPlainTextPayload("Acknowledged"))
+	// 	res := canopus.NewResponse(msg, nil)
+	// 	return res
+	// })
 
 	server.Get("/time", func(req canopus.Request) canopus.Response {
 		now := time.Now()
@@ -45,23 +47,46 @@ func main() {
 		return res
 	})
 
-	server.Get("/basic/json", func(req canopus.Request) canopus.Response {
-		msg := canopus.NewMessageOfType(canopus.MessageAcknowledgment, req.GetMessage().GetMessageId(), nil)
-
+	server.Get("/watch/this", func(req canopus.Request) canopus.Response {
+		msg := canopus.NewMessageOfType(canopus.MessageAcknowledgment, req.GetMessage().GetMessageId(), canopus.NewPlainTextPayload("Acknowledged"))
 		res := canopus.NewResponse(msg, nil)
 
 		return res
 	})
+	ticker := time.NewTicker(3 * time.Second)
+	go func() {
+		for {
+			select {
+			case <-ticker.C:
+				changeVal := strconv.Itoa(rand.Int())
+				fmt.Println("[SERVER << ] Change of value -->", changeVal)
 
-	server.Get("/basic/xml", func(req canopus.Request) canopus.Response {
-		msg := canopus.NewMessageOfType(canopus.MessageAcknowledgment, req.GetMessage().GetMessageId(), nil)
-		res := canopus.NewResponse(msg, nil)
+				server.NotifyChange("/watch/this", changeVal, false)
+			}
+		}
+	}()
 
-		return res
-	})
+	// server.Get("/basic/json", func(req canopus.Request) canopus.Response {
+	// 	msg := canopus.NewMessageOfType(canopus.MessageAcknowledgment, req.GetMessage().GetMessageId(), nil)
+
+	// 	res := canopus.NewResponse(msg, nil)
+
+	// 	return res
+	// })
+
+	// server.Get("/basic/xml", func(req canopus.Request) canopus.Response {
+	// 	msg := canopus.NewMessageOfType(canopus.MessageAcknowledgment, req.GetMessage().GetMessageId(), nil)
+	// 	res := canopus.NewResponse(msg, nil)
+
+	// 	return res
+	// })
 
 	server.OnMessage(func(msg canopus.Message, inbound bool) {
 		canopus.PrintMessage(msg)
+	})
+
+	server.OnObserve(func(resource string, msg canopus.Message) {
+		fmt.Println("[SERVER << ] Observe Requested for " + resource)
 	})
 
 	server.ListenAndServe(":5683")
